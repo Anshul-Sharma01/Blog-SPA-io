e2eafa80230b1991b183ca7f37615a5836d6973b
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-08-27 14:21:06 +0530

    completed favourites controllers and routes
    changed addToFavourite and removeFromFavourite controller to single toggleFavourite controller

--- server/src/controllers/favourites.controller.js
+++ server/src/controllers/favourites.controller.js
@@ -7,62 +7,121 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 
-const addToFavourite = asyncHandler(async(req, res, next) => {
+
+const toggleFavourite = asyncHandler(async( req, res, next) => {
     try{
         const { blogId } = req.params;
-        const userId = req.user._id;
         const { blogUserId } = req.body;
-        
-        if(!isValidObjectId(blogId)){
-            throw new ApiError(400, "Invalid Blog Id");
-        }
+        const userId = req.user._id;
 
-        const blogFavourite = await Favourites.create({
-            blogOwner : blogUserId,
-            owner : userId,
-            blog : blogId
-        })
-
-        if(!blogFavourite){
-            throw new ApiError(400, "Some Error occurred while adding blog to favourites list");
-        }
-
-        return res.status(201).json(new ApiResponse(201, blogFavourite, "Blog added to Favourites"));
-
-    }catch(err){
-        throw new ApiError(400, err?.message || "Error occurred while adding to favourites");
-    }
-})
-
-const removeFromFavourite = asyncHandler(async (req, res, next) => {
-    try{
-        const { blogId } = req.params;
-        const  userId = req.user._id;
 
         if(!isValidObjectId(blogId)){
             throw new ApiError(400,"Invalid Blog Id");
         }
 
-        const blogExists = await Favourites.findOne({ blog : blogId, owner : userId });
-
-        if(!blogExists){
-            throw new ApiError(400, "Blog is already not in the favourites list");
+        if(!isValidObjectId(blogUserId)){
+            throw new ApiError(400, "Invalid User Id");
         }
 
-        await blogExists.deleteOne();
-        return res.status(200).json(new ApiResponse(200, [], "Blog removed from favourites"));
+        const blogExists = await Favourites.findOne({ blog: blogId, owner : userId });
+
+        if(blogExists){
+            await blogExists.deleteOne();
+            return res.status(200).json(new ApiResponse(200, blogExists, "Blog removed from Favourites"));
+        }
+
+        const favBlog = await Favourites.create({
+            blog : blogId,
+            owner : userId,
+            blogOwner : blogUserId
+        })
+
+        return res.status(201).json(new ApiResponse(201, favBlog, "Blog added to Favourites"));
+
 
 
     }catch(err){
-        throw new ApiError(400, err?.message || "Error occurred while removing blog from favourites");
+        throw new ApiError(400, err?.message || "Error occurred while toggling Favourite");
+    }
+})
+
+const getAllFavourites = asyncHandler( async (req, res, next) => {
+    try{
+        const userId = req.user._id;
+
+        const favouriteBlogs = await Favourites.find({owner : userId});
+
+        if(favouriteBlogs.length === 0){
+            return res.status(200).json(new ApiResponse(200, [], "You haven't added any blog to favourites"));
+        }
+        return res.status(200).json(new ApiResponse(200, favouriteBlogs, "Favourite blogs fetched"));
+
+    }catch(err){    
+        throw new ApiError(400, err?.message || "Error occurred while fetching favourite blogs");
+    }
+})
+
+const getFavouriteCountForBlog = asyncHandler(async (req, res, next) => {
+    try{
+        const { blogId } = req.params;
+
+        if(!isValidObjectId(blogId)){
+            throw new ApiError(400, "Invalid Blog Id");
+        }
+
+        const countFavourites = await Favourites.find({blog : blogId}).countDocuments();
+
+        if(countFavourites === 0){
+            return res.status(200).json(new ApiResponse(200, [], "No-one has favourited yet"));
+        }
+
+        return res.status(200).json(new ApiResponse(200, countFavourites, "Fetched count of stars" ));
+
+
+    }catch(err){
+        throw new ApiError(400, err?.message || "Error occurred while fetching count of favourites");
+    }
+})
+
+const clearAllFavourites = asyncHandler(async ( req, res, next) => {
+    try{
+        const userId = req.user._id;
+
+        await Favourites.deleteMany({ owner : userId });
+
+        return res.status(200).json(new ApiResponse(200, [], "Favourites list wiped off"));
+
+
+    }catch(err){
+        throw new ApiError(400, err?.message ||  "Error occurred while removing all favourites");
+    }
+})
+
+const getFavouriteBlogsByOwner = asyncHandler(async(req, res,next) => {
+    try{
+        const { blogUserId } = req.params;
+    
+        if(!isValidObjectId(blogUserId)){
+            throw new ApiError(400, "Invalid Blog Owner Id");
+        }
+
+        const favBlogs = await Favourites.find({ blogOwner : blogUserId }).populate("blog");
+        if(favBlogs.length === 0){
+            return res.status(200).json(new ApiResponse(200, [], "No Favourite blogs found"));
+        }
+
+        return res.status(200).json(new ApiResponse(200, favBlogs, "Favourite blogs fetched"));
+
+    }catch(err){
+        throw new ApiError(400, err?.message || "Error occurred while finding favourite blogs of a particular user");
     }
 })
 
 
-
-
-
 export{
-    addToFavourite,
-    removeFromFavourite
+    toggleFavourite,
+    getAllFavourites,
+    getFavouriteCountForBlog,
+    clearAllFavourites,
+    getFavouriteBlogsByOwner
 }
--- server/src/routes/favourites.routes.js
+++ server/src/routes/favourites.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addToFavourite, removeFromFavourite } from "../controllers/favourites.controller.js";
+import { addToFavourite, clearAllFavourites, getAllFavourites, getFavouriteBlogsByOwner, getFavouriteCountForBlog, removeFromFavourite, toggleFavourite } from "../controllers/favourites.controller.js";
 
 
 
@@ -8,8 +8,12 @@ const router = Router();
 
 app.use(verifyJWT);
 
-router.route("/add/:blogId").post(addToFavourite);
-router.route("/remove/:blogId").delete(removeFromFavourite);
+router.route("/").get(getAllFavourites);
+router.route("/fav/:blogId").post(toggleFavourite);
+router.route("/countfav/:blogId").post(getFavouriteCountForBlog);
+router.route("/clear").delete(clearAllFavourites);
+router.route("/:blogUserId").get(getFavouriteBlogsByOwner);
+
 
 export default router;
 
