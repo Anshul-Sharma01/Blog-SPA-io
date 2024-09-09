fc06a84ff06b1576802575001604cdb356ac967e
Anshul Sharma <sharma.ansh2926@gmail.com>
2024-09-09 17:58:33 +0530

    setup of reduxtoolkit and react-router-dom done

---
+++ client/src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+
+function HomePage(){
+    return(
+        <>
+            <h1 className="bg-slate-400 text-4xl text-black">Home-Page</h1>
+        </>
+    )
+}
+
+
+export default HomePage;
+
+
