be1694c29b2697da747e946a05ed9c5cc8ea056a
Anshul Sharma <sharma.ansh2926@gmail.com>
2024-09-11 16:14:17 +0530

    Added axios Interceptors for handling accessToken updation

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -1,12 +1,66 @@
 import axios from "axios";
-
+import Cookies from "js-cookie";
 
 const BASE_URL = "http://localhost:8000/api/v1";
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true,
+});
 
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;
+axiosInstance.interceptors.request.use(
+    async (config) => {
+        const accessToken = Cookies.get('accessToken');
+        const refreshToken = Cookies.get('refreshToken');
+
+        if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response) {
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+            const refreshToken = Cookies.get('refreshToken');
+            const response = await axiosInstance.post('/users/refresh-token', {
+                refreshToken,
+            });
+
+            const newAccessToken = response.data.accessToken;
+            const newRefreshToken = response.data.refreshToken;
+
+            Cookies.set('accessToken', newAccessToken);
+            Cookies.set('refreshToken', newRefreshToken);
+
+            originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+            return axiosInstance(originalRequest);
+            } catch (err) {
+            console.error('Error refreshing access token:', err);
+            return Promise.reject(err);
+            }
+        }
+        } else {
+        console.error('Error with the request:', error.message);
+        }
+
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
-
