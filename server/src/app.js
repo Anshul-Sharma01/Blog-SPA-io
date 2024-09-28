44d07a70b41c523cab1ea783aea7dec778d47114
Anshul Sharma <sharma.ansh2926@gmail.com>
2024-09-28 18:59:01 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ import morgan from "morgan";
 const app = express();
 
 app.use(cors({
-    origin : [process.env.FRONTEND_URL],
+    origin : 'https://blog-spa-io.vercel.app/,
     credentials : true
 }));
 
