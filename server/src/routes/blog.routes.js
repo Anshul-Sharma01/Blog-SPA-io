d492ba7bf3b71675505a21ad6d78d5220290c7f4
Anshul Sharma <sharma.ansh2926@gmail.com>
2024-11-07 12:13:31 +0530

    Added functionality to integrate fetching of latest blogs and also to fetch blogs on the basis of search query at backend

--- server/src/routes/blog.routes.js
+++ server/src/routes/blog.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyAdmin, verifyJWT } from "../middlewares/auth.middleware.js";
-import { createBlog, deleteBlog, fetchAllBlogs, fetchBlogsByCategory, fetchMyBlogs, updateBlogDetails, updateBlogThumbnail, viewBlog } from "../controllers/blog.controller.js";
+import { createBlog, deleteBlog, fetchAllBlogs, fetchBlogsByCategory, fetchBlogsByTitleOrCategory, fetchLatestBlogsOfAuthor, fetchMyBlogs, updateBlogDetails, updateBlogThumbnail, viewBlog } from "../controllers/blog.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 
@@ -8,7 +8,9 @@ import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
 router.route("/viewall").get(fetchAllBlogs);
+router.route("/view/search").get(fetchBlogsByTitleOrCategory);
 router.route("/view/category/:category").get(fetchBlogsByCategory);
+router.route("/view/author-blogs/:authorId").get(fetchLatestBlogsOfAuthor);
 router.route("/view/:blogId").get(viewBlog);
 router.route("/my").get(verifyJWT, fetchMyBlogs);
 router.route("/create").post(verifyJWT, upload.single("thumbnail"), createBlog);
